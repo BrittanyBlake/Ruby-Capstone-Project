48a9e679157314fddcf3b2e520450edad93a661f
Brittany Blake <brittany.blake49@gmail.com>
2020-04-24 09:40:01 +0200

    Fix pagination

--- scraper.rb
+++ scraper.rb
@@ -13,8 +13,7 @@ class Scraper
 
     per_page = smartphone_listings.count
     total = parsed_page.css('hgroup.cf').css('h1').css('em').text.split('')[1..3].join('').to_i
-    last_page = (total / per_page.to_f).round
-   
+    last_page = (total / per_page).round
 
     while page <= last_page
       pagination_url = "https://www.mediamarkt.es/es/category/_smartphones-701189.html?searchParams=&sort=&view=PRODUCTLIST&page=#{page}"
@@ -23,17 +22,20 @@ class Scraper
       puts "page: #{page}"
       puts ''
 
-      pagination_smartphone_listings = parsed_page.css('div.product-wrapper')
+      pagination_unparsed_page = HTTParty.get(pagination_url)
+      pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page)
+      pagination_smartphone_listings = pagination_parsed_page.css('div.product-wrapper')
 
       pagination_smartphone_listings.each do |smartphone_listing|
         smartphone = {
-          name: smartphone_listing.css('div.content').css('h2').css('a').text.strip.split.drop(2).join(' '),
+          description: smartphone_listing.css('div.content').css('h2').text.strip.split.drop(2).join(' ').gsub(/-/, ''),
           price: '€' + smartphone_listing.css('div.price-box').css('div.small').text,
           url: 'https://www.mediamarkt.es' + smartphone_listing.css('div.content').css('a')[0].attributes['href'].value
         }
 
         smartphones << smartphone
-        puts " #{smartphone[:name]} costs #{smartphone[:price]}"
+        puts ''
+        puts " #{smartphone[:description]} costs #{smartphone[:price]}"
         puts ''
       end
       page += 1
