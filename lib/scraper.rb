f907a7eb2a7ff2de0630007b1010af5ff2ad08b6
Brittany Blake <brittany.blake49@gmail.com>
2020-04-26 13:09:21 +0200

    Add phone_info method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,5 @@
+
+
 require 'nokogiri'
 require 'httparty'
 require 'byebug'
@@ -14,6 +16,8 @@ class Scraper
   end
 
   # rubocop:disable Metrics/AbcSize
+
+  #split def scrape
   def scrape
     smartphone_listings = parsed_page.css('div.product-wrapper')
     page = 1
@@ -21,7 +25,7 @@ class Scraper
     per_page = smartphone_listings.count
     total = parsed_page.css('hgroup.cf').css('h1').css('em').text.split('')[1..3].join('').to_i
     last_page = (total / per_page.to_f).ceil
-
+    
     puts "Total number of smartphones: #{total}".blue
     puts "Total number of pages: #{last_page}".blue
     puts "Number of Smartphones per page:#{per_page}".blue
@@ -34,6 +38,7 @@ class Scraper
       puts ''
 
       pagination_unparsed_page = HTTParty.get(pagination_url)
+      #TSE SAYS HERE SHOULD BE PRIVATE
       pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page)
       pagination_smartphone_listings = pagination_parsed_page.css('div.product-wrapper')
 
@@ -43,14 +48,29 @@ class Scraper
           price: '€' + smartphone_listing.css('div.price-box').css('div.small').text,
           url: 'https://www.mediamarkt.es' + smartphone_listing.css('div.content').css('a')[0].attributes['href'].value
         }
-
+        #TSE THINKS HERE SHOULD END PRIVATE
+        csv(smartphones)
         smartphones << smartphone
-        puts ''
-        puts " #{smartphone[:description]} costs #{smartphone[:price]}".red
-        puts ''
+        phone_info(smartphone)
+       
       end
       page += 1
     end
   end
+
+  def phone_info(smartphone)
+    puts ''
+    puts " #{smartphone[:description]} costs #{smartphone[:price]}".red
+    puts ''
+  end
+
+  def csv(smartphones)
+    CSV.open("phones.csv", "w") do |csv|
+      smartphones.each do |phone|
+        csv << [phone[:description], phone[:price]] 
+      end
+    end
+  end
   # rubocop:enable Metrics/AbcSize
 end
+
